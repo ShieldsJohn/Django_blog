f5a19e17e953801de641ab3cc75fcc4fcc28c6ee
John Shields <shields101@hotmail.com>
2024-03-08 17:55:26 +0000

    Authentication and register page

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'SECRET_KEY = os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-shieldsjohn-djangoblog-7cmr0yafi21.ws-eu108.gitpod.io','.herokuapp.com']
 
